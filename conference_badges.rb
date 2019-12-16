a28fce86c11ea23c4fe8de5b176af59a08b7c58c
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-16 03:51:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees) 
+  badge_names = []
+   attendees.each do |attendee|
+    badge_names << "Hello, my name is #{attendee}."
+   end
+   badge_names
+end
+
+def batch_badge_creator(attendees)
+    attendees.collect do |attendees|
+      badge_maker(attendee)
+    end
+end
+
+def assign_rooms
+  
+end
+
